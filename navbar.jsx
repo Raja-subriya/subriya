6b7fcee32a0e4071d18047faa6480ec7fefa60ce
Raja-subriya <168518815+Raja-subriya@users.noreply.github.com>
2025-08-19 14:56:02 +0530

    Create navbar.jsx

---
+++ navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">Voting App</h1>
+      <div className="links">
+        <Link to="/">Voting</Link>
+        <Link to="/results">Results</Link>
+      </div>
+    </nav>
+  );
